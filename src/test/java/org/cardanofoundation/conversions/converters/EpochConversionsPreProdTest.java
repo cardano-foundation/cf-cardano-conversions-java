55f96269e8ac758b144a9e2c237c82f80d6720f7
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2023-11-23 13:25:27 +0100

    Fix: added extra pre-prod unit test.

--- src/test/java/org/cardanofoundation/conversions/converters/EpochConversionsPreProdTest.java
+++ src/test/java/org/cardanofoundation/conversions/converters/EpochConversionsPreProdTest.java
@@ -1,15 +1,16 @@
 package org.cardanofoundation.conversions.converters;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.cardanofoundation.conversions.domain.EpochOffset.END;
-import static org.cardanofoundation.conversions.domain.EpochOffset.START;
-import static org.cardanofoundation.conversions.domain.NetworkType.PREPROD;
-
-import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.cardanofoundation.conversions.ClasspathConversionsConfigFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cardanofoundation.conversions.domain.EpochOffset.END;
+import static org.cardanofoundation.conversions.domain.EpochOffset.START;
+import static org.cardanofoundation.conversions.domain.NetworkType.PREPROD;
 
 @Slf4j
 class EpochConversionsPreProdTest {
@@ -32,5 +33,11 @@ class EpochConversionsPreProdTest {
   public void testEpochBabbage108StartTime() {
     assertThat(epochConversions.epochToUTCTime(108, START))
         .isEqualTo(LocalDateTime.of(2023, 11, 23, 0, 0, 0));
+  }
+
+  @Test
+  public void testEpochBabbage103EndTime() {
+    assertThat(epochConversions.epochToUTCTime(103, END))
+            .isEqualTo(LocalDateTime.of(2023, 11, 3, 0, 0, 0));
   }
 }
