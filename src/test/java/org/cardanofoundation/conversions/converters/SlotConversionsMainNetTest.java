7ce62e1e90b90b2d8c1173b3f3fc30c08e18c363
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2024-05-14 23:33:07 +0100

    chore: shelley slot to epoch conversion

--- src/test/java/org/cardanofoundation/conversions/converters/SlotConversionsMainNetTest.java
+++ src/test/java/org/cardanofoundation/conversions/converters/SlotConversionsMainNetTest.java
@@ -1,14 +1,15 @@
 package org.cardanofoundation.conversions.converters;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.cardanofoundation.conversions.domain.NetworkType.MAINNET;
-
-import java.time.LocalDateTime;
 import lombok.extern.slf4j.Slf4j;
 import org.cardanofoundation.conversions.ClasspathConversionsFactory;
 import org.cardanofoundation.conversions.GenesisConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cardanofoundation.conversions.domain.NetworkType.MAINNET;
 
 @Slf4j
 class SlotConversionsMainNetTest {
@@ -47,4 +48,28 @@ class SlotConversionsMainNetTest {
     assertThat(slotConversions.slotToTime(slot))
         .isEqualTo(LocalDateTime.of(2023, 11, 22, 12, 47, 9));
   }
+
+  @Test
+  public void testFirstShelleySlot() {
+    var slot = 4492800L;
+    assertThat(slotConversions.slotToEpoch(slot)).isEqualTo(208);
+  }
+  @Test
+  public void testEpoch208() {
+    var slot = 4492801L;
+    assertThat(slotConversions.slotToEpoch(slot)).isEqualTo(208);
+  }
+  @Test
+  public void testEpoch209() {
+    var slot = 4492800L + genesisConfig.getShelleyEpochLength();
+    assertThat(slotConversions.slotToEpoch(slot)).isEqualTo(209);
+  }
+  @Test
+  public void testEpoch300() {
+    var slot = 44237054L;
+    assertThat(slotConversions.slotToEpoch(slot)).isEqualTo(300);
+  }
+
+
+
 }
