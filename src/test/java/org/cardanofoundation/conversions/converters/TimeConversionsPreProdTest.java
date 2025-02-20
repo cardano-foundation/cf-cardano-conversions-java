92cb9b6f2731ff7f0257175ae56b012f76ebe7b6
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2025-02-20 20:46:22 +0100

    chore: adding more tests

--- src/test/java/org/cardanofoundation/conversions/converters/TimeConversionsPreProdTest.java
+++ src/test/java/org/cardanofoundation/conversions/converters/TimeConversionsPreProdTest.java
@@ -7,6 +7,8 @@ import java.time.LocalDateTime;
 import org.cardanofoundation.conversions.ClasspathConversionsFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class TimeConversionsPreProdTest {
 
@@ -28,5 +30,13 @@ class TimeConversionsPreProdTest {
   public void testBabbageEra2() {
     assertThat(timeConversions.utcTimeToEpochNo(LocalDateTime.of(2023, 11, 23, 12, 48, 58)))
         .isEqualTo(108);
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    "2024-11-29T15:51:43,77208703" // https://preprod.cardanoscan.io/transaction/1188eb4283ae74e4e62d99ec201cb15ee916d4edb2b70fc8de3cb2824ea461b7
+  })
+  public void dateToSlot(String dateTime, long absoluteSlot) {
+    assertThat(timeConversions.toSlot(LocalDateTime.parse(dateTime))).isEqualTo(absoluteSlot);
   }
 }
